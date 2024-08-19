c00b963f567148ca32f4284d25ea7ee98b0d1b5b
MitchTansey <143773715+MitchTansey@users.noreply.github.com>
2024-08-19 19:39:38 +0000

    Testing diff timer values

--- heap_sort/heap_sort.c
+++ heap_sort/heap_sort.c
@@ -65,7 +65,7 @@ int main() {
     uint32_t result;
 
     __asm__ volatile (
-        "addi x1, zero, 1000\n"  // Adds 1000 to zero and stores it in x1 (result)
+        "addi x1, zero, 500\n"  // Adds 1000 to zero and stores it in x1 (result)
         ".word 0xa00e00b" // Custom instruction
         : "=r" (result)  // Output operand: %0 corresponds to x1 (result will be stored here)
         :                // No input operands
