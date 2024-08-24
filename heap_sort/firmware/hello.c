1fe3103167182616eea2a53ab89c9c817b636b33
MitchTansey <143773715+MitchTansey@users.noreply.github.com>
2024-08-24 21:53:44 +0000

    trying hello method

--- heap_sort/firmware/hello.c
+++ heap_sort/firmware/hello.c
@@ -9,7 +9,7 @@
 
 void hello(void)
 {
-	uint32_t address = 0x1000; // Example memory address
+    uint32_t address = 0x1000; // Example memory address
 	uint32_t value_s = 0x0F00;
 
 	__asm__ volatile (
@@ -19,5 +19,13 @@ void hello(void)
 		"r" (address)  // Input operand: %1 (address) will be stored in a register
     );
 
-	print_str("Stored value in 0x1000\n");
+    uint32_t result;
+
+    __asm__ volatile (
+        "addi x1, zero, 500\n"  // Adds 1000 to zero and stores it in x1 (result)
+        ".word 0xa00e00b" // Custom instruction
+        : "=r" (result)  // Output operand: %0 corresponds to x1 (result will be stored here)
+        :                // No input operands
+        : "x1"           // Clobber list: indicate that x1 is modified
+    );
 }
