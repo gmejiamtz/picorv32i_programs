5ce4348fe5f6ef7e791d5ad67e7493b1502952db
gmejiamtz <gmejiama@ucsc.edu>
2025-03-09 23:50:56 -0700

    This fails the hexfile python script, comment out print_hex for it to work

--- bubble_sort/firmware/firmware.h
+++ bubble_sort/firmware/firmware.h
@@ -9,7 +9,7 @@
 #define FIRMWARE_H
 
 #include <stdint.h>
-#include <stdbool.h>
+//#include <stdbool.h>
 
 // irq.c
 //uint32_t *irq(uint32_t *regs, uint32_t irqs);
@@ -17,7 +17,7 @@
 // print.c
 void print_chr(char ch);
 void print_str(const char *p);
-//void print_dec(unsigned int val);
+void print_dec(unsigned int val);
 void print_hex(unsigned int val, int digits);
 
 // hello.c
