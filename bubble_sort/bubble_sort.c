70d991b318cb423cf3e423114a8979e46ead2180
gmejiamtz <gmejiama@ucsc.edu>
2025-03-10 02:04:40 -0700

    Bubble sort rn sorts 16 items

--- bubble_sort/bubble_sort.c
+++ bubble_sort/bubble_sort.c
@@ -1,6 +1,6 @@
 #include "firmware/firmware.h"
 
-#define ARRAY_SIZE 4096
+#define ARRAY_SIZE 64
 //array held in speacial mem section of ram
 int arr[ARRAY_SIZE] __attribute__((section(".sorter_section")));
 
@@ -24,9 +24,17 @@ void bubbleSort(int arr[], int n) {
 
 // Main function
 void main() {
+    print_str("Sorting 64 elements\n");
     for (int i = 0; i < ARRAY_SIZE; i++) {
         arr[i] = (ARRAY_SIZE - i);  // Reverse order for worst-case scenario
     }
     bubbleSort(arr, ARRAY_SIZE);
     //call the uart module here for all items in the 
+    print_str("Sorted the Array:\n");
+    for(int i = 0; i < ARRAY_SIZE; i++) {
+	print_dec(arr[i]);
+	print_chr(',');
+    }
+    print_chr(']');
+    print_str("\nProgram Terminated...\n");
 }
